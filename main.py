a3e8ab7bd9e002735f50b84113f4190fe380e2b1
Sim <sarostegui.ctes@gmail.com>
2025-06-13 16:39:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mapa_frame = ttk.LabelFrame(main_frame, text="Mapa")
 mapa_frame.pack(side = "left", fill="both", expand=True, padx=5, pady=5)
 mapa = TkinterMapView(mapa_frame, width=600, height=500, corner_radius=0)
 mapa.pack(fill="both", expand=True)
-mapa.set_position(-32.9510139, -60.6260167, "TEST")
+mapa.set_position(-32.9510139, -60.6260167)
 mapa.set_zoom(15)
 
 root.mainloop()
